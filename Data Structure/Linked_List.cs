9e46478aeb89159090a650e031b640e64b148304
VinayDanagoud <vinaydanagoud143@gmail.com>
2022-07-12 17:43:03 +0530

    Updated Linked List

--- Data Structure/Linked_List.cs
+++ Data Structure/Linked_List.cs
@@ -9,30 +9,37 @@ namespace DataStructure
     internal class Linked_List<Gtype>
     {
 
-
         // variable
+
 
         Node<Gtype> head = null;
 
         public void Add(Gtype data)
         {
+            // variable
+            // store the count in tenp node
+
             Node<Gtype> temp_node;
 
             // create new node with data
             Node<Gtype> node = new Node<Gtype>(data);
 
-
+            // there is no node
             if (head == null)
             {
+                // if there is no element so first one is head
                 head = node;
                 return;
+
             }
             else
             {
+                //count start from head
                 temp_node = head;
-
+                // check for null
                 while (temp_node.next != null)
                     temp_node = temp_node.next;
+                // adding the node to next variable      
                 temp_node.next = node;
             }
         }
@@ -47,7 +54,9 @@ namespace DataStructure
             }
             else
             {
+                //created temp variable and the node display start from head
                 Node<Gtype> temp_node = head;
+                //displaying head as we had head value
                 Console.WriteLine(temp_node.val);
                 while (temp_node.next != null)
                 {
