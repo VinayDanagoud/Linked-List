8262740aba87fcf96314673c8939a2badd5fcf60
VinayDanagoud <vinaydanagoud143@gmail.com>
2022-07-12 17:51:16 +0530

    Appened Linked List

--- Data Structure/Linked_List.cs
+++ Data Structure/Linked_List.cs
@@ -8,38 +8,29 @@ namespace DataStructure
 {
     internal class Linked_List<Gtype>
     {
-
         // variable
-
 
         Node<Gtype> head = null;
 
         public void Add(Gtype data)
         {
-            // variable
-            // store the count in tenp node
-
             Node<Gtype> temp_node;
 
             // create new node with data
             Node<Gtype> node = new Node<Gtype>(data);
 
-            // there is no node
+
             if (head == null)
             {
-                // if there is no element so first one is head
                 head = node;
                 return;
-
             }
             else
             {
-                //count start from head
                 temp_node = head;
-                // check for null
+
                 while (temp_node.next != null)
                     temp_node = temp_node.next;
-                // adding the node to next variable      
                 temp_node.next = node;
             }
         }
@@ -54,9 +45,7 @@ namespace DataStructure
             }
             else
             {
-                //created temp variable and the node display start from head
                 Node<Gtype> temp_node = head;
-                //displaying head as we had head value
                 Console.WriteLine(temp_node.val);
                 while (temp_node.next != null)
                 {
@@ -65,5 +54,20 @@ namespace DataStructure
                 }
             }
         }
+        public void AddFirstNode(Gtype data)
+        {
+            Node<Gtype> newNode = new Node<Gtype>(data);
+            newNode.next = head;
+            head = newNode;
+            Console.WriteLine("{0} : Nodes inserted in Linked list ", newNode.val);
+        }
+        //Method to Append Linked List
+        public void AppendLinked_List(Gtype data)
+        {
+            Add(data);
+            Console.WriteLine("{0} node Appended", data);
+        }
+
+
     }
 }
